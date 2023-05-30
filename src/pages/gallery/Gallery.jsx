fc57a90be1f6352cfbaef9239895d8230d225c70
Rajeev Baniya <rajeevbaniya11@gmail.com>
2023-05-30 11:47:48 +0545

    change in image

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 import "./gallery.css";
 import useFetch from "../../hooks/useFetch";
 import { Link } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
 const Gallery = () => {
   const [query, setQuery] = useState("");
   const [page, setPage] = useState(1);
   const apiKey = process.env.REACT_APP_CLIENT;
   const endPoint = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${apiKey}`;
 
-  const { data, loading, error, reFetch } = useFetch(endPoint);
+  const { data } = useFetch(endPoint);
 
   return (
     <div className="gallery">
@@ -30,7 +32,7 @@ const Gallery = () => {
         {data.results &&
           data?.results.map((item) => (
             <div className="col-lg-3 col-md-4 col-sm-6" key={item.id}>
-              <img src={item.urls.regular} alt={query} />
+              <LazyLoadImage src={item.urls.regular} alt={query} />
             </div>
           ))}
       </div>
